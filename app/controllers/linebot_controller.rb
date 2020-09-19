87fcd7d4027695b86b7c99d022227d83ec1c9a0f
shhmd92 <sho.hamada.92@gmail.com>
2020-09-19 15:24:29 +0900

    [mod]create_message method

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -5,9 +5,33 @@ class LinebotController < ApplicationController
 
   protect_from_forgery except: [:callback]
 
-  BURNABLE = %W[\u751F\u3054\u307F \u7D19\u3054\u307F \u30B4\u30E0\u88FD\u54C1\u985E \u30D7\u30E9\u30B9\u30C1\u30C3\u30AF\u985E \u9769\u88FD\u54C1 \u98DF\u7528\u6CB9 \u8863\u985E].freeze
-  NON_BURNABLE = %W[\u91D1\u5C5E\u985E \u30AC\u30E9\u30B9\u985E \u9676\u78C1\u5668\u985E 30\u30BB\u30F3\u30C1\u672A\u6E80\u306E\u5BB6\u96FB\u88FD\u54C1].freeze
-  RECYCLABLE = %W[\u65B0\u805E \u96D1\u8A8C \u6BB5\u30DC\u30FC\u30EB \u3073\u3093 \u7F36 \u30DA\u30C3\u30C8\u30DC\u30C8\u30EB \u7D19\u30D1\u30C3\u30AF \u7D19\u7BB1 \u7D19\u888B OA\u7528\u7D19].freeze
+  BURNABLE = %w[
+    生ごみ
+    紙ごみ
+    ゴム製品類
+    プラスチック類
+    革製品
+    食用油
+    衣類
+  ].freeze
+  NON_BURNABLE = %w[
+    金属類
+    ガラス類
+    陶磁器類
+    30センチ未満の家電製品
+  ].freeze
+  RECYCLABLE = %w[
+    新聞
+    雑誌
+    段ボール
+    びん
+    缶
+    ペットボトル
+    紙パック
+    紙箱
+    紙袋
+    OA用紙
+  ].freeze
 
   def client
     @client ||= Line::Bot::Client.new do |config|
@@ -41,11 +65,11 @@ class LinebotController < ApplicationController
 
   def create_message(event)
     seatch_text = event.message['text']
-    if BURNABLE.select { |e| e =~ /.*#{seatch_text}.*/ }
+    if BURNABLE.include?(seatch_text)
       '可燃ゴミ'
-    elsif NON_BURNABLE.select { |e| e =~ /.*#{seatch_text}.*/ }
+    elsif NON_BURNABLE.include?(seatch_text)
       '不燃ゴミ'
-    elsif RECYCLABLE.select { |e| e =~ /.*#{seatch_text}.*/ }
+    elsif RECYCLABLE.include?(seatch_text)
       '資源ゴミ'
     else
       '不明'
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -11,9 +13,6 @@ module GarbageSeparationBot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
   end
 end
